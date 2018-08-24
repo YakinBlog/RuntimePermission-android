15ba8307bb8a2492ccb05e63dd949f3eeafcbb45
Yakin Wong <yakin.wyj@gmail.com>
2018-08-24 11:39:45 +0800

    打印授权结果

--- app/src/main/java/com/yakin/rtp/simple/MainActivity.java
+++ app/src/main/java/com/yakin/rtp/simple/MainActivity.java
@@ -5,30 +5,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.yakin.rtp.IRTPGrantCallback;
 import com.yakin.rtp.RTPManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mResultView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.record).setOnClickListener(new View.OnClickListener() {
+        mResultView = (TextView) findViewById(R.id.result);
+
+        findViewById(R.id.internet).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RTPManager.requestPermissions(MainActivity.this, Manifest.permission.RECORD_AUDIO, new IRTPGrantCallback() {
+                RTPManager.requestPermissions(MainActivity.this, Manifest.permission.INTERNET, new IRTPGrantCallback() {
 
                     @Override
                     public void onPermissionGranted() {
                         Log.d("--RTP--", "onPermissionGranted");
+                        mResultView.setText("INTERNET:onPermissionGranted");
                     }
 
                     @Override
                     public void onPermissionDenied() {
                         Log.d("--RTP--", "onPermissionDenied");
+                        mResultView.setText("INTERNET:onPermissionDenied");
+                    }
+                });
+            }
+        });
+
+        findViewById(R.id.change).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RTPManager.requestPermissions(MainActivity.this, Manifest.permission.CHANGE_WIFI_STATE, new IRTPGrantCallback() {
+
+                    @Override
+                    public void onPermissionGranted() {
+                        Log.d("--RTP--", "onPermissionGranted");
+                        mResultView.setText("CHANGE_WIFI_STATE:onPermissionGranted");
+                    }
+
+                    @Override
+                    public void onPermissionDenied() {
+                        Log.d("--RTP--", "onPermissionDenied");
+                        mResultView.setText("CHANGE_WIFI_STATE:onPermissionDenied");
                     }
                 });
             }
@@ -42,15 +69,58 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onPermissionGranted() {
                         Log.d("--RTP--", "onPermissionGranted");
+                        mResultView.setText("WRITE_EXTERNAL_STORAGE:onPermissionGranted");
                     }
 
                     @Override
                     public void onPermissionDenied() {
                         Log.d("--RTP--", "onPermissionDenied");
+                        mResultView.setText("WRITE_EXTERNAL_STORAGE:onPermissionDenied");
                     }
                 });
             }
         });
+
+        findViewById(R.id.phone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RTPManager.requestPermissions(MainActivity.this, Manifest.permission.READ_PHONE_STATE, new IRTPGrantCallback() {
+
+                    @Override
+                    public void onPermissionGranted() {
+                        Log.d("--RTP--", "onPermissionGranted");
+                        mResultView.setText("READ_PHONE_STATE:onPermissionGranted");
+                    }
+
+                    @Override
+                    public void onPermissionDenied() {
+                        Log.d("--RTP--", "onPermissionDenied");
+                        mResultView.setText("READ_PHONE_STATE:onPermissionDenied");
+                    }
+                });
+            }
+        });
+
+        findViewById(R.id.record).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RTPManager.requestPermissions(MainActivity.this, Manifest.permission.RECORD_AUDIO, new IRTPGrantCallback() {
+
+                    @Override
+                    public void onPermissionGranted() {
+                        Log.d("--RTP--", "onPermissionGranted");
+                        mResultView.setText("RECORD_AUDIO:onPermissionGranted");
+                    }
+
+                    @Override
+                    public void onPermissionDenied() {
+                        Log.d("--RTP--", "onPermissionDenied");
+                        mResultView.setText("RECORD_AUDIO:onPermissionDenied");
+                    }
+                });
+            }
+        });
+
         findViewById(R.id.camera).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,11 +129,13 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onPermissionGranted() {
                         Log.d("--RTP--", "onPermissionGranted");
+                        mResultView.setText("CAMERA:onPermissionGranted");
                     }
 
                     @Override
                     public void onPermissionDenied() {
                         Log.d("--RTP--", "onPermissionDenied");
+                        mResultView.setText("CAMERA:onPermissionDenied");
                     }
                 });
             }
